1e1656c734a02e155a8ea79835868e11eeaca082
Jonathan Araldi <jonathan.araldi@outlook.com>
2024-05-24 19:53:34 +0200

    Correction des derniers messages d'erreurs dans la console

--- src/components/Icon/index.test.js
+++ src/components/Icon/index.test.js
@@ -3,16 +3,30 @@ import md5 from "md5";
 import Icon from ".";
 
 describe("Icon component", () => {
-    describe("When a icon is created with name twitch", () => {
-        it("the icon contain this path hash value 327fbc38c8e878259c3ec35ef231517a", () => {
-            render(<Icon name="twitch" />)
-            expect(md5(screen.getByTestId("icon").getAttribute('d'))).toEqual('327fbc38c8e878259c3ec35ef231517a')
+    describe("When a icon is created", () => {
+        it("with name twich it has this path hash value 327fbc38c8e878259c3ec35ef231517a", () => {
+            render(<Icon name="twitch" />);
+            expect(md5(screen.getByTestId("icon-twich").getAttribute("d"))).toEqual(
+            "327fbc38c8e878259c3ec35ef231517a"
+            );
+        });
+        // Implémentation des test sur les autres icônes
+        it("with name facebbok it has this path hash value bbea4c9e40773b969fdb6e406059f853", () => {
+            render(<Icon name="facebook" />);
+            expect(
+            md5(screen.getByTestId("icon-facebook").getAttribute("d"))
+            ).toEqual("bbea4c9e40773b969fdb6e406059f853");
+        });
+        it("with name twitter it has this path hash value 82f5be4a5c07199cb75dacec50b90b2a", () => {
+            render(<Icon name="twitter" />);
+            expect(md5(screen.getByTestId("icon-twitter").getAttribute("d"))).toEqual(
+            "82f5be4a5c07199cb75dacec50b90b2a");
+        });
+        it("with name youtube it has this path hash value 43342876c2fc40e5b2afe621443ff95b", () => {
+            render(<Icon name="youtube" />);
+            expect(md5(screen.getByTestId("icon-youtube").getAttribute("d"))).toEqual(
+            "43342876c2fc40e5b2afe621443ff95b");
         });
     });
-    describe("When a icon is created with name facebook", () => {
-        it("the icon contain this path hash value bbea4c9e40773b969fdb6e406059f853", () => {
-            // to complete
-        });
-    });
-})
+});
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -116,13 +116,15 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
-        <EventCard
-          imageSrc={last?.cover}
-          title={last?.title}
-          date={new Date(last?.date)}
-          small
-          label="boom"
-        />
+        {
+        last && (
+          <EventCard
+            imageSrc={last.cover}
+            title={last.title}
+            date={new Date(last.date)}
+            label={last?.type}
+          />
+        )}
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
